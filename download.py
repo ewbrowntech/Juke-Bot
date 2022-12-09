112c9b826f1f38cc10c73f270cc8e6a08f44e136
Ethan Brown <ewbrowntech@gmail.com>
2022-12-09 01:15:49 -0600

    Bot can now handle download command being passed without A* YouTube URL included

--- download.py
+++ download.py
@@ -2,14 +2,15 @@ from pytube import YouTube
 import re
 
 async def download_command(message):
+    # Detect whether a YouTube URL is present within the command
     match = re.search(r'https://www.youtube.com/\S+|https://youtu.be/\S+', message.content)
     if match:
-        # Extract the YouTube link from the match
         ytLink = match.group(0)
-
-        # Handle the YouTube link as needed
         print("Found a YouTube link: " + ytLink)
-    await message.channel.send('Downloading YouTube video...\n' + ytLink)
+        await message.channel.send('Downloading YouTube video...\n' + ytLink)
+    else:
+        print("No YouTube URL detected.")
+        await message.channel.send('No YouTube URL detected.')
 
 
 def download_video(url):
